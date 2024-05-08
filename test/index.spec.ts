2b2515090e5dd1aaf38792699cc63de17bb0171a
hypernova1 <hypemova@gmail.com>
2024-05-08 18:55:09 +0900

    test: 테스트 코드 추가

---
+++ test/index.spec.ts
@@ -0,0 +1,65 @@
+import { createSwaggerDefaultDto, updateSwaggerDto } from '../src';
+import { ApiProperty } from '@nestjs/swagger';
+
+class Post {
+    @ApiProperty({
+        description: 'post title',
+        default: 'hello',
+        required: true,
+    })
+    title: string | undefined;
+
+    @ApiProperty({
+        description: 'post content',
+        default: 'world',
+        required: false,
+
+    })
+    content: string | undefined;
+}
+
+class User {
+    @ApiProperty({
+        description: 'name',
+        default: 'melchor',
+        required: true,
+    })
+    name: string | undefined;
+
+    @ApiProperty({
+        description: 'post list',
+        type: Post,
+        default: Post,
+        required: false,
+    })
+    posts: Post[] | undefined;
+}
+
+
+describe('test', () => {
+    it('create default instance', () => {
+        const instance = createSwaggerDefaultDto(User);
+        expect(instance.name).toBeDefined();
+    });
+
+    it('create default instance without required fields', () => {
+        const instance = createSwaggerDefaultDto(User, false);
+        expect(instance.posts).toBeUndefined();
+    });
+
+    it('create instance and get array property', () => {
+        const instance = createSwaggerDefaultDto(User);
+        expect(instance.posts).toBeDefined();
+    });
+
+    it('create instance and update instance', () => {
+        const instance = createSwaggerDefaultDto(User);
+        const preName = instance.name;
+        updateSwaggerDto(instance, {
+            name: 'morgoth',
+        });
+        const updatedTitle = instance.name;
+
+        expect(preName).not.toEqual(updatedTitle);
+    });
+});
